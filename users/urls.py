fd95cf8a1369b9dfc301e9f7140f0d3ce6232133
10Escape01 <selviarlinda@gmail.com>
2025-10-17 21:32:14 +0300

    dop

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('balance/', CheckBalanceView.as_view(), name='balance'),
     path('add-points/', AddPointsView.as_view(), name='add-points'),
-    path('register-page/', register_page, name='register-page'),  # Новый маршрут для страницы регистрации
+    path('register-page/', register_page, name='register-page'),  # Маршрут для HTML-страницы регистрации
 ]
