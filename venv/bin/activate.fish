e2b7cbcdf2d6dca375ffe468a58bfb9b547a6f68
10Escape01 <selviarlinda@gmail.com>
2025-10-17 20:45:56 +0300

    create base

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/admi/projects/TGminiAPP/telegram-crypto-app/venv
+set -gx VIRTUAL_ENV /home/admi/projects/TGminiAPP/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
