86fbcea5ed2a685febab4663af2d16b3b5df01bf
10Escape01 <selviarlinda@gmail.com>
2025-10-18 14:08:08 +0300

    REBOOT

--- users/models.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-class CustomUser(AbstractUser):
-    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-
-    def __str__(self):
-        return self.username
